4aff0e19ca17ccf5ed9b63d6a51113f6cf229f08
Junior Damacena <juniordamacena980@gmail.com>
2017-06-07 15:25:32 -0300

    The page of licences now show only the title related to the language that is passed as parameter

--- js/licencas.js
+++ js/licencas.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    var language = getUrlParameter('lang');
-    alert("Alert");
+    var language = getUrlParameter("lang");
+
     switch (language) {
         case "en":
-            $(".titulo-pagina[class!='en']").hide();
+            $(".titulo-pagina > [class!='en']").hide();
             break;
         case "pt-BR":
         default:
-            $(".titulo-pagina[class!='pt-BR']").hide();
+            $(".titulo-pagina > [class!='pt-BR']").hide();
             break;
     }
 });
